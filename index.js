1337d3986b260e8dc88a89f10148e2d66aa70585
akshit5230 <43376244+akshit5230@users.noreply.github.com>
2020-04-02 11:34:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,96 +1,24 @@
 import React from 'react';
 import {
-  View, Text,
-  AppRegistry,
-  StyleSheet,
-  processColor
+  View,
 } from 'react-native';
-import { BarChart, Grid, YAxis, XAxis } from 'react-native-svg-charts';
-import * as scale from 'd3-scale';
 import * as d3 from 'd3';
-import { BubbleChart } from 'react-native-charts-wrapper';
-import update from 'immutability-helper';
-import _ from 'lodash';
 import Svg, {
   Circle,
   G,
   Text as SVGText,
 } from 'react-native-svg';
 
-export class TestBubble extends React.Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-
-    };
-  }
+export default class BubbleChart extends React.Component {
 
   render() {
-
-    let data = [
-      {
-        name: "analytics",
-        title: "flare/analytics",
-        group: "analytics",
-        value: 0,
-        color: '#42826C'
-      },
-      {
-        name: "lorem",
-        title: "flare/analytics",
-        group: "analytics",
-        value: 10,
-        color: '#42D2C6'
-      },
-      {
-        name: "standard",
-        title: "flare/analytics",
-        group: "analytics",
-        value: 120,
-        color: '#8AD242'
-      },
-      {
-        name: "ipsum",
-        title: "flare/analytics",
-        group: "analytics",
-        value: 70,
-        color: '#F88888'
-      },
-      {
-        name: "analytics",
-        title: "flare/analytics",
-        group: "analytics",
-        value: 20,
-        color: '#666699'
-      },
-      {
-        name: "wow",
-        title: "flare/analytics",
-        group: "analytics",
-        value: 10,
-        color: '#CC5CA2'
-      },
-      {
-        name: "great",
-        title: "flare/analytics",
-        group: "analytics",
-        value: 56,
-        color: '#609EF1'
-      },
-      {
-        name: "analytics",
-        title: "flare/analytics",
-        group: "analytics",
-        value: 4,
-        color: '#FFBC67'
-      }
-    ]
 
     const {
       height,
       width,
+      data,
+      textProps,
+      circleProps
     } = this.props;
 
     let pack = data => d3.pack()
@@ -118,14 +46,14 @@ export class TestBubble extends React.Component {
       leaves.push(
         <G transform={`translate(${leaf.x + 1},${leaf.y + 1})`}>
           <Circle
+            {...circleProps}
             r={leaf.r}
             fill={leaf.data.color}
           />
           <SVGText
+            {...textProps}
             fill="#FFFFFF"
-            // stroke="purple"
             fontSize={fontSizeGenerator(leaf.data.value)}
-            // fontWeight="bold"
             x="0"
             y={leaf.data.value * 0.1}
             textAnchor="middle" >{leaf.data.name}</SVGText>
